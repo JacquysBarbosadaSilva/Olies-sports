7fa514963af49f0d8abefd8a81f5cbfa5d6e6d95
JacquysBarbosadaSilva <jacquys0801@gmail.com>
2025-10-16 11:42:18 -0300

    Rota das informações o produto

--- olies-sports/src/routes/route.js
+++ olies-sports/src/routes/route.js
@@ -8,69 +8,83 @@ import HomeScreen from "../screens/HomeScreen";
 import Perfil from "../screens/Perfil";
 import Categorias from "../screens/Categorias";
 import Produtos from "../screens/Produtos";
+import InfoProduto from "../screens/InfoProduto";
 
 const Tab = createBottomTabNavigator();
 const Stack = createNativeStackNavigator();
 
 function TabNavigator() {
-  return (
-    <Tab.Navigator
-      screenOptions={({ route }) => ({
-        tabBarIcon: ({ focused, color, size }) => {
-          let iconName;
+    return (
+        <Tab.Navigator
+            screenOptions={({ route }) => ({
+                tabBarIcon: ({ focused, color, size }) => {
+                    let iconName;
 
-          if (route.name === "Home") {
-            iconName = focused ? "home" : "home-outline";
-          } else if (route.name === "Perfil") {
-            iconName = focused ? "person" : "person-outline";
-          } else if (route.name === "Categorias") {
-            iconName = focused ? "list" : "list-outline";
-          }
+                    if (route.name === "Home") {
+                        iconName = focused ? "home" : "home-outline";
+                    } else if (route.name === "Perfil") {
+                        iconName = focused ? "person" : "person-outline";
+                    } else if (route.name === "Categorias") {
+                        iconName = focused ? "list" : "list-outline";
+                    }
 
-          return <Ionicons name={iconName} size={size} color={color} />;
-        },
-        tabBarActiveTintColor: "#052242", // Cor quando ativo
-        tabBarInactiveTintColor: "#A3A3A3", // Cor quando inativo
-        headerShown: false,
-      })}
-    >
-      <Tab.Screen name="Home" component={HomeScreen} />
+                    return <Ionicons name={iconName} size={size} color={color} />;
+                },
+                tabBarActiveTintColor: "#052242", // Cor quando ativo
+                tabBarInactiveTintColor: "#A3A3A3", // Cor quando inativo
+                headerShown: false,
+            })}
+        >
+            <Tab.Screen name="Home" component={HomeScreen} />
 
-      <Tab.Screen name="Categorias" component={Categorias} />
+            <Tab.Screen name="Categorias" component={Categorias} />
 
-      <Tab.Screen name="Perfil" component={Perfil} />
-    </Tab.Navigator>
-  );
+            <Tab.Screen name="Perfil" component={Perfil} />
+        </Tab.Navigator>
+    );
 }
 
 function AppNavigation() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen
-          name="Tabs"
-          component={TabNavigator}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="Produtos"
-          component={Produtos}
-          options={({ navigation }) => ({
-            headerShown: true,
-            title: "Produtos",
-            headerLeft: () => (
-              <Ionicons
-                name="arrow-back"
-                size={24}
-                style={{ marginLeft: 15 }}
-                onPress={() => navigation.goBack()}
-              />
-            ),
-          })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
+    return (
+        <NavigationContainer>
+            <Stack.Navigator screenOptions={{ headerShown: false }}>
+                <Stack.Screen name="Tabs" component={TabNavigator} options={{ headerShown: false }} />
+                <Stack.Screen
+                    name="Produtos"
+                    component={Produtos}
+                    options={({ navigation }) => ({
+                        headerShown: true,
+                        title: "Produtos",
+                        headerLeft: () => (
+                            <Ionicons
+                                name="arrow-back"
+                                size={24}
+                                style={{ marginLeft: 15 }}
+                                onPress={() => navigation.goBack()}
+                            />
+                        ),
+                    })}
+                />
+
+                <Stack.Screen
+                    name="InfoProduto"
+                    component={InfoProduto}
+                    options={({ navigation }) => ({
+                        headerShown: true,
+                        title: "Info Produto",
+                        headerLeft: () => (
+                            <Ionicons
+                                name="arrow-back"
+                                size={24}
+                                style={{ marginLeft: 15 }}
+                                onPress={() => navigation.goBack()}
+                            />
+                        ),
+                    })}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
 }
 
 export default AppNavigation;
